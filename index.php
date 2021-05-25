0f18979fbeee9dfefceb2bcdac9340545d7402f9
Atsushi Kato <atsushikato@AtsushinoMacBook-Pro.local>
2021-05-25 22:33:47 +0900

    sm

--- index.php
+++ index.php
@@ -1,21 +1,18 @@
 <?php
 
-$url = 'https://api.line.me/v2/oauth/accessToken';
 
-// POSTデータ
+//チャネルアクセストークンを取得する
+$url = 'https://api.line.me/v2/oauth/accessToken';
 $data = array(
     "grant_type" => "client_credentials",
     "client_id" => "1655993509",
     "client_secret" => "f065711da54cc0949483b9b1448d8a75",
 );
 $data = http_build_query($data, "", "&");
-
-// header
 $header = array(
     "Content-Type: application/x-www-form-urlencoded",
     "Content-Length: ".strlen($data)
 );
-
 $context = array(
     "http" => array(
         "method"  => "POST",
@@ -23,40 +20,58 @@ $context = array(
         "content" => $data
     )
 );
-
 $json = file_get_contents($url, false, stream_context_create($context));
 $arr = json_decode($json, true);
-
-//echo $json
 $access_token = $arr['access_token'];
-echo $access_token
+echo $access_token ;
 
-/* $url = 'https://api.line.me/message/v3/notifier/token';
 
-// POSTデータ
+//サービス通知トークンを取得する
 $data = array(
-    "liffAccessToken" => "1",
-);
-$data = http_build_query($data, "", "&");
-
-// header
-$header = array(
-    "Content-Type: application/json",
-    'Authorization: Bearer LWRqBo5PflLofo4At25WXs4pAW7b7h4bMLH0eASO8CNuHSU8VPY9qQxtZbfhjuxBEAlLZ8uHJxSIF6PbJV2+Arrfh3pDv9Y6r2fNq937FUw0YMPIor8ly/7sy7WAm9Vigy+YMeM61H0TqzpksKU+i49PbdgDzCFqoOLOYbqAITQ=',
-);
-
-$context = array(
+    "liffAccessToken" => "eyJhbGciOiJIUzI1NiJ9.UOdHAINGR72S6ay8kqpoA1gZ5EEbaBe-h7_wyCbyvVbz6crobC0nWuVkgVJMq_MSodWpbQczc51-1XpeyU8ahO29sX-_cFgN4LSfr67hS5sI527-AfM1bfPnGbEDFfwaY0V83aL5Znqx28HLSVCF9KEDhsbGwDj72yG4nagw8Go.kCn0K4g1m4lcB0f5KI_AejWUgXe0m7dYYOL3U-qw3x4",
+  );
+  $data = json_encode($data);
+  $header = array(
+    "Content-Type: application/json; charset=UTF-8",
+    'Authorization: Bearer '. $access_token,
+    );
+  $options = array(
     "http" => array(
-        "method"  => "POST",
-        "header"  => implode("\r\n", $header),
-        "content" => $data
+      "method" => "POST",
+      "header"  => implode("\r\n", $header),
+      "content" => $data
     )
-);
+  );
+  $context = stream_context_create($options);
+  $json = file_get_contents("https://api.line.me/message/v3/notifier/token", false, $context);
+  $arr = json_decode($json, true);
+  $notificationToken = $arr['notificationToken'];
+  echo $notificationToken;
 
-$html = file_get_contents($url, false, stream_context_create($context));
 
-echo $html; */
-
-
+  //サービスメッセージを送る
+  $params = array(
+    "number" => "1357"
+  );
+  $data = array(
+    "templateName" => "order_comp_s_o_ja",
+    "params" => "",
+    "notificationToken" => $notificationToken,
+  );
+  $data = json_encode($data);
+  echo   $data;
+  $header = array(
+    "Content-Type: application/json; charset=UTF-8",
+    'Authorization: Bearer '. $access_token,
+    );
+  $options = array(
+    "http" => array(
+      "method" => "POST",
+      "header"  => implode("\r\n", $header),
+      "content" => $data
+    )
+  );
+  $context = stream_context_create($options);
+  $json = file_get_contents("https://api.line.me/message/v3/notifier/send?target=service", false, $context);
 
 ?>
