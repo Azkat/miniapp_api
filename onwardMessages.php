b215e7bd273ef80781a893aeb366828e91b17b83
Atsushi Kato <atsushikato@AtsushinoMacBook-Pro.local>
2021-06-04 20:47:39 +0900

    database

--- onwardMessages.php
+++ onwardMessages.php
@@ -2,32 +2,7 @@
   header('Access-Control-Allow-Origin: *');
 
   $notification_token = $_GET['notification_token']; 
-
-
-  //チャネルアクセストークンを取得する
-  $url = 'https://api.line.me/v2/oauth/accessToken';
-  $data = array(
-      "grant_type" => "client_credentials",
-      "client_id" => "1655993509",
-      "client_secret" => "f065711da54cc0949483b9b1448d8a75",
-  );
-  $data = http_build_query($data, "", "&");
-  $header = array(
-      "Content-Type: application/x-www-form-urlencoded",
-      "Content-Length: ".strlen($data)
-  );
-  $context = array(
-      "http" => array(
-          "method"  => "POST",
-          "header"  => implode("\r\n", $header),
-          "content" => $data
-      )
-  );
-  $json = file_get_contents($url, false, stream_context_create($context));
-  $arr = json_decode($json, true);
-  $access_token = $arr['access_token'];
-  echo $access_token ;
-  
+  $channel_access_token = $_GET['channel_access_token']; 
 
   //後続メッセージを送信する
   $params = array(
@@ -42,7 +17,7 @@
   echo   $data;
   $header = array(
     "Content-Type: application/json; charset=UTF-8",
-    'Authorization: Bearer '. $access_token,
+    'Authorization: Bearer '. $channel_access_token,
     );
   $options = array(
     "http" => array(
