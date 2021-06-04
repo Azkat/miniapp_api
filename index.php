5a1531de75183d60d751647c9292b369324be179
Atsushi Kato <atsushikato@AtsushinoMacBook-Pro.local>
2021-06-04 16:35:59 +0900

    database

--- index.php
+++ index.php
@@ -79,4 +79,29 @@ $data = array(
   $context = stream_context_create($options);
   $json = file_get_contents("https://api.line.me/message/v3/notifier/send?target=service", false, $context);
 
+  //db接続
+  $user = 'root';
+  $pass = 'secret';
+  
+  try {
+      // MySQLへの接続
+      $dbh = new PDO('mysql:host=db;dbname=test', $user, $pass);
+  
+      // 接続を使用する
+      $sth = $dbh->query('SELECT * from fooo');
+      echo "<pre>";
+      foreach($sth as $row) {
+          print_r($row);
+      }
+      echo "</pre>";
+  
+      // 接続を閉じる
+      $sth = null;
+      $dbh = null;
+  
+  } catch (PDOException $e) { // PDOExceptionをキャッチする
+      print "エラー!: " . $e->getMessage() . "<br/gt;";
+      die();
+  }
+
 ?>
